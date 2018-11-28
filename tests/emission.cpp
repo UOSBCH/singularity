a11ff7234c8957ef62678b601522631b9e36f8e9
Alexey Prokopov <prokopov.alexey@gmail.com>
2018-11-28 17:17:35 +0300

    Unit test for emission_calculator_new

--- tests/emission.cpp
+++ tests/emission.cpp
@@ -15,11 +15,11 @@ std::vector<transaction_t> get_transactions1()
 
     time_t now = time(nullptr);
     
-    transactions.push_back( transaction_t (200, 0, "account-0", "account-1", now, 0, 0));
-    transactions.push_back( transaction_t (100, 0, "account-1", "account-0", now, 0, 0));
-    transactions.push_back( transaction_t (300, 0, "account-0", "account-2", now, 0, 0));
-    transactions.push_back( transaction_t (500, 0, "account-2", "account-1", now, 0, 0));
-    transactions.push_back( transaction_t (700, 0, "account-1", "account-2", now, 0, 0));
+    transactions.push_back( transaction_t (200, 0, "account-0", "account-1", now, 0, 0, 0));
+    transactions.push_back( transaction_t (100, 0, "account-1", "account-0", now, 0, 0, 0));
+    transactions.push_back( transaction_t (300, 0, "account-0", "account-2", now, 0, 0, 0));
+    transactions.push_back( transaction_t (500, 0, "account-2", "account-1", now, 0, 0, 0));
+    transactions.push_back( transaction_t (700, 0, "account-1", "account-2", now, 0, 0, 0));
     
     return transactions;
 }
@@ -30,13 +30,13 @@ std::vector<transaction_t> get_transactions2()
 
     time_t now = time(nullptr);
     
-    transactions.push_back( transaction_t (200, 0, "account-0", "account-1", now, 0, 0));
-    transactions.push_back( transaction_t (100, 0, "account-1", "account-0", now, 0, 0));
-    transactions.push_back( transaction_t (300, 0, "account-0", "account-2", now, 0, 0));
-    transactions.push_back( transaction_t (500, 0, "account-2", "account-1", now, 0, 0));
-    transactions.push_back( transaction_t (700, 0, "account-1", "account-2", now, 0, 0));
-    transactions.push_back( transaction_t (800, 0, "account-1", "account-3", now, 0, 0));
-    transactions.push_back( transaction_t (100, 0, "account-2", "account-3", now, 0, 0));
+    transactions.push_back( transaction_t (200, 0, "account-0", "account-1", now, 0, 0, 0));
+    transactions.push_back( transaction_t (100, 0, "account-1", "account-0", now, 0, 0, 0));
+    transactions.push_back( transaction_t (300, 0, "account-0", "account-2", now, 0, 0, 0));
+    transactions.push_back( transaction_t (500, 0, "account-2", "account-1", now, 0, 0, 0));
+    transactions.push_back( transaction_t (700, 0, "account-1", "account-2", now, 0, 0, 0));
+    transactions.push_back( transaction_t (800, 0, "account-1", "account-3", now, 0, 0, 0));
+    transactions.push_back( transaction_t (100, 0, "account-2", "account-3", now, 0, 0, 0));
     
     return transactions;
 }
@@ -167,5 +167,44 @@ BOOST_AUTO_TEST_CASE( test3 )
 
     BOOST_CHECK_EQUAL(ap.get_activity(), ap2.get_activity());
 }
+
+BOOST_AUTO_TEST_CASE( new_emission_test )
+{
+    double_type current_supply = 100000000;
+    double_type emission_event_count_per_year = 12;
+    double_type year_emission_limit = 10;
+    double_type emission_scale = 100000;
+    double_type emission_period = double_type(365*24*3600)/emission_event_count_per_year;
+    double_type delay_koefficient = 0.5;
+    
+    double_type current_activity, max_activity, emission_limit, target_emission, emission;
+    
+    emission_calculator_new ec;
+    
+    current_activity = 3;
+    max_activity = 0;
+
+    emission_limit = ec.get_emission_limit(current_supply, year_emission_limit, emission_period);
+    BOOST_CHECK_CLOSE(emission_limit, 797414.04289, 1e-3);
+    target_emission = ec.get_target_emission(current_activity, max_activity, emission_scale);
+    BOOST_CHECK_CLOSE(target_emission, 300000, 1e-3);
+    emission = ec.get_resulting_emission(target_emission, emission_limit, delay_koefficient);
+    BOOST_CHECK_CLOSE(emission, 148255, 1e-3);
+    
+    current_supply += emission;
+    max_activity = current_activity;
+    current_activity = 5;
+
+    emission_limit = ec.get_emission_limit(current_supply, year_emission_limit, emission_period);
+    BOOST_CHECK_CLOSE(emission_limit, 798596.2526, 1e-3);
+    target_emission = ec.get_target_emission(current_activity, max_activity, emission_scale);
+    BOOST_CHECK_CLOSE(target_emission, 200000, 1e-3);
+    emission = ec.get_resulting_emission(target_emission, emission_limit, delay_koefficient);
+    
+    BOOST_CHECK_CLOSE(emission, 99480.5915, 1e-3);
+//    BOOST_CHECK_CLOSE(emission, 173083, 1e-3);
+    
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
