1c38e91933f468b49e10fe1ac5021f0f0ce7b3e5
Alexey Prokopov <prokopov.alexey@gmail.com>
2019-06-13 20:08:00 +0300

    Alternative PageRank implementation

---
+++ include/page_rank_alt.hpp
@@ -0,0 +1,48 @@
+
+#ifndef PAGE_RANKING_ALT_HPP
+#define PAGE_RANKING_ALT_HPP
+
+#include "rank_interface.hpp"
+#include "utils.hpp"
+
+namespace singularity {
+    class page_rank_alt: public rank_interface
+    {
+    public:
+        page_rank_alt(double_type outlink_weight, uint16_t num_threads, double_type precision)
+            :outlink_weight(outlink_weight), 
+            num_threads(num_threads),
+            precision(precision)
+            {};
+        const uint32_t MAX_ITERATIONS = 1000;
+        void set_outlink_weight(double_type a_outlink_weight) {
+            outlink_weight = a_outlink_weight;
+        };
+        virtual std::shared_ptr<vector_t> process(
+            const matrix_t& outlink_matrix,
+            const vector_t& initial_vector,
+            const vector_t& weight_vector,
+            const additional_matrices_vector& additional_matrices
+        );
+    private:
+        double_type outlink_weight;
+        uint16_t num_threads;
+        double_type precision;
+        virtual std::shared_ptr<vector_t> calculate_rank(
+            const matrix_t& outlink_matrix, 
+            const additional_matrices_vector& additional_matrices,
+            const vector_t& initial_vector,
+            const vector_t& weight_vector
+        );
+        virtual std::shared_ptr<vector_t> iterate(
+            const matrix_t& outlink_matrix, 
+            const additional_matrices_vector& additional_matrices,
+            const vector_t& previous,
+            const vector_t& teleportation
+        );
+    };
+}
+
+#endif /* PAGE_RANKING_ALT_HPP */
+
+
---
+++ page_rank_alt.cpp
@@ -0,0 +1,66 @@
+#include "include/page_rank_alt.hpp"
+#include <boost/numeric/ublas/io.hpp>
+
+using namespace boost;
+using namespace boost::numeric::ublas;
+using namespace singularity;
+
+std::shared_ptr<vector_t> page_rank_alt::process(
+        const matrix_t& outlink_matrix,
+        const vector_t& initial_vector,
+        const vector_t& weight_vector,
+        const additional_matrices_vector& additional_matrices
+) {
+    return calculate_rank(outlink_matrix, additional_matrices, initial_vector, weight_vector);
+}
+
+std::shared_ptr<vector_t> page_rank_alt::iterate(
+        const matrix_t& outlink_matrix, 
+        const additional_matrices_vector& additional_matrices,
+        const vector_t& previous,
+        const vector_t& teleportation
+) {
+    std::shared_ptr<vector_t> next(new vector_t(outlink_matrix.size1(), 0));
+    
+    matrix_tools::prod(*next, outlink_matrix, previous, num_threads);
+    
+    for (auto additional_matrix: additional_matrices) {
+        *next += prod(*additional_matrix, previous) * outlink_weight;
+    }
+    
+//     *next += teleportation;
+    
+    return next;
+}
+
+std::shared_ptr<vector_t> page_rank_alt::calculate_rank(
+        const matrix_t& outlink_matrix, 
+        const additional_matrices_vector& additional_matrices,
+        const vector_t& initial_vector,
+        const vector_t& weight_vector
+) {
+    std::shared_ptr<vector_t> next;
+    std::shared_ptr<vector_t> result = std::make_shared<vector_t>(weight_vector.size(), 0);
+    vector_t teleportation(weight_vector * (double_type(1) - outlink_weight));
+    std::shared_ptr<vector_t> previous = std::make_shared<vector_t>(teleportation);
+    
+    *result += *previous;
+    
+    matrix_t outlink_matrix_weighted = outlink_matrix * outlink_weight;
+    
+    for (uint i = 0; i < MAX_ITERATIONS; i++) {
+        next  = iterate(outlink_matrix_weighted, additional_matrices, *previous, teleportation);
+        double_type norm = norm_1(*next);
+
+        if (norm <= precision) {
+            return result;
+        } else {
+            previous = next;
+            *result += *next;
+        }
+    }
+    
+    return result;
+}
+
+
