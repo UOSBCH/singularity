98895d4609fc94490df211cda53f242518f16961
Alexey Prokopov <prokopov.alexey@gmail.com>
2019-10-30 11:25:28 +0300

    Allow set const_contribution to 0

--- social_index_calculator.cpp
+++ social_index_calculator.cpp
@@ -697,10 +697,9 @@ void social_index_calculator::add_phantom_account_relations (matrix_t& m)
         for (auto i: account_map) {
             auto name = i.first;
             auto id = i.second;
-            
-             if (id != phantom_account_id) {
-                m(*phantom_account_id, id) = 1;
-             }
+//            if (id != phantom_account_id) {
+            m(*phantom_account_id, id) = 1;
+//            }
         }
     }
 }
